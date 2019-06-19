4404de80ac8dd6f64d5c9f39b921cb45d56b45c1
snmaddula <snmaddula@gmail.com>
2019-06-19 14:02:23 +0530

    Added main class

--- src/main/java/snmaddula/aws/lambda/App.java
+++ src/main/java/snmaddula/aws/lambda/App.java
@@ -1,10 +1,30 @@
 package snmaddula.aws.lambda;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
+import lombok.Setter;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+@Setter
 @SpringBootApplication
-public class App {
+@Import(AppContext.class)
+public class App extends SpringBootRequestHandler<String, String> {
+
+	@Bean
+	public Function<String, String> retrieveSecrets(SecretsService service) {
+		return secretId -> service.retrieveSecrets(secretId);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
