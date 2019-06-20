cdbc0b56762604937eaeb9af882cf72177c7e0ef
snmaddula <snmaddula@gmail.com>
2019-06-20 13:55:41 +0530

    updated App.java

--- src/main/java/snmaddula/aws/lambda/App.java
+++ src/main/java/snmaddula/aws/lambda/App.java
@@ -1,15 +1,17 @@
 package snmaddula.aws.lambda;
 
+import java.util.Properties;
 import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import lombok.Setter;
+import snmaddula.aws.lambda.domain.SecretRequest;
+import snmaddula.aws.lambda.service.SecretsService;
 
 /**
  * 
@@ -19,11 +21,11 @@ import lombok.Setter;
 @Setter
 @SpringBootApplication
 @Import(AppContext.class)
-public class App extends SpringBootRequestHandler<String, String> {
+public class App extends SpringBootRequestHandler<SecretRequest, Properties> {
 
 	@Bean
-	public Function<String, String> retrieveSecrets(SecretsService service) {
-		return secretId -> service.retrieveSecrets(secretId);
+	public Function<SecretRequest, Properties> retrieveSecrets(SecretsService service) {
+		return req -> service.retrieveSecrets(req.getSecretId());
 	}
 
 	public static void main(String[] args) {
